8af2508ce42d9875779e14b075bedfebd7c156c2
code-alt <54744445+code-alt@users.noreply.github.com>
2020-10-13 18:23:09 -0500

    Performance of life changes
    I changed some of the wording and code you had to make life easier.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let database = new jsoning("sql.json");
 
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`You're logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
@@ -23,12 +23,12 @@ client.on('message', msg => {
     
         if(json.reports !== ""){
             if(typeof json.reports == 'undefined'){
-                msg.reply("Invalid user.")
+                msg.reply("I can't read how much reports this user has, because of an error. The person might not exist.")
             }else{
                 msg.reply(`User requested has **${json.reports}** reports and has an abuse score of **${json.score}**. See more about ths user at https://discord.riverside.rocks/check?id=${id} . If you feel that this user is abusive, please report them at https://discord.riverside.rocks/report?id=${id} .`)
             }
         }else{
-            msg.reply("Invalid user.")
+            msg.reply("Invalid user. Maybe there was an error?")
         }
     })
 
@@ -39,17 +39,17 @@ client.on('message', msg => {
     var id = msg.content.substr(8);
     (async() => {
       if(db.has(message.guild.id)){
-        if(!message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')){
-          msg.reply("We are sorry, but you do not have the permisions to do this.")
+        if(message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')){ // i personally believe that using ! is a bad practice
+         
         }else{
-          // POST a request to the server with the API key that the server has set
+         msg.reply("We are so sorry, but you do not have the permission to do this. https://http.cat/401")
         }
       }else{
         if(id == ""){
-          msg.reply("This server does not have an API key assciated with it. In a secure channel, please use with command with your API key. (ex. `!report abcdefg`) To get your accounts API Key, sign in at https://discord.riverside.rocks/login and view it on the dashboard.");
+          msg.reply("This server does not have an API key assciated with it. In a secure channel where NO ONE ELSE is there, please use with command with your API key. (ex. `!report abcdefg`) To get your accounts API Key, sign in at https://discord.riverside.rocks/login to view it on the dashboard.");
         }else{
           await db.push(message.guild.id, id);
-          msg.reply("Your API key has been saved.");
+          msg.reply("Your API key has been saved to our database.");
         }
       }
     })
